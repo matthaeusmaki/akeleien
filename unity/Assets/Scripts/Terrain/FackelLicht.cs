dd1eb98efe1f26eb0338073b1794cef67a0f7a57
uNreaLilein <thomas.echerer@gmail.com>
2016-01-03 15:12:46 +0100

    Update auf Unity 5.3 | Hinzufügen aller Models
    Enthält außerdem:
    - den Versuch eines ersten Levels
    - eine selbstgemachte Fackel
    
    Sounds sind größtenteils ausgelagert um das Projekt nicht allzugroß zu
    machen.

---
+++ unity/Assets/Scripts/Terrain/FackelLicht.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FackelLicht : MonoBehaviour {
+	
+	public float m_IntensitiyMin = 3;
+	public float m_IntensitiyMax = 4;
+	public float m_RangeMin = 6;
+	public float m_RangeMax = 7;
+	public float m_biggestStep = 0.01f;
+	public float m_jumpMax = 0.05f;
+
+	private Vector3 m_originalPosition;
+
+	private Light light;
+
+	// Use this for initialization
+	void Start () {
+		light = GetComponent<Light> ();
+		m_originalPosition = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		setRandomLightRange ();
+		setRandomLihgtIntensitiy ();
+		setRandomPosition ();
+	}
+
+	private void setRandomPosition() {		
+		float newYPosition = Random.Range (m_originalPosition.y - m_jumpMax, m_originalPosition.y + m_jumpMax);
+		Vector3 newPosition = new Vector3(m_originalPosition.x, newYPosition, m_originalPosition.z);
+		transform.position = Vector3.MoveTowards (transform.position, newPosition, 1);
+	}
+
+	private void setRandomLightRange() {
+		float newRange;
+		int minusRange = Random.Range(0,10);
+		if (minusRange < 5) {			
+			newRange = Random.Range (light.intensity - m_biggestStep, light.intensity); 
+			if (newRange < m_RangeMin) 
+				newRange = m_RangeMin;			
+		} else {			
+			newRange = Random.Range (light.range, light.range + m_biggestStep); 
+			if (newRange > m_RangeMax) 
+				newRange = m_RangeMax;			
+		}
+		light.range = newRange;
+	}
+
+	private void setRandomLihgtIntensitiy() {
+		int minusIntensity = Random.Range (0, 10);
+
+		float newIntensitiy;
+
+		if (minusIntensity < 5) {
+			newIntensitiy = Random.Range (light.intensity - m_biggestStep, light.intensity);
+			if (newIntensitiy < m_IntensitiyMin)
+				newIntensitiy = m_IntensitiyMin;
+		} else {
+			newIntensitiy = Random.Range (light.intensity, light.intensity + m_biggestStep);
+			if (newIntensitiy < m_IntensitiyMax)
+				newIntensitiy = m_IntensitiyMax;
+		}
+
+		light.intensity = newIntensitiy;
+	}
+}
---
+++ unity/Assets/Scripts/character/MoveToClick.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MoveToClick : MonoBehaviour {
+
+    private int speed = 50;
+    private NavMeshAgent agent;
+    
+
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+        //  Bei Mausklick
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Debug.Log("Mausklick registriert");
+            //  Ermittle Ziel
+            Vector3 ziel = findTargetPoint();
+            try
+            {
+                Debug.Log("Bewege Spieler zu (X=" + ziel.x + ") (z=" + ziel.z);
+                agent.SetDestination(ziel);
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e);
+            }
+            
+        }
+        
+	}
+
+    private Vector3 findTargetPoint()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject.name.Equals("ground"))
+            {
+                return hit.point;
+            }
+        }
+        throw new Exception("Boden nicht geunfen !");
+    }
+}
