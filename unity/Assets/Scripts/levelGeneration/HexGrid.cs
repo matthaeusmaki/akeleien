ed7304b6c6b99979f7b32280491e221a9374ff6f
Matthaeus Baur <matthaeus.baur@makiart.de>
2016-03-28 00:42:01 +0200

    hex grid Editor (gizmo)

---
+++ unity/Assets/Scripts/levelGeneration/HexGrid.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HexGrid : MonoBehaviour {
+
+
+    public float tileWidth = 10.0f;
+    public int rows = 10;
+    public int columns = 10;
+
+    public Color color = Color.red;
+
+	void Start () {
+	}
+	
+	void Update () {
+	}
+
+    void OnDrawGizmos() {
+        Gizmos.color = color;
+        Vector3 pos = this.transform.position;
+
+        float gridWidth = columns * tileWidth;
+
+        for (int row = 0; row <= rows; row++) {
+            float posZ = pos.z + row * tileWidth;
+
+            // x-Richtung
+            Gizmos.DrawLine(new Vector3(pos.x + (rows % 2 == 0 && row == rows ? tileWidth/2 : 0), 0.0f, posZ),
+                        new Vector3(pos.x + gridWidth + (row == 0 || (rows %2 != 0 && row == rows) ? 0 : tileWidth / 2), 0.0f, posZ));
+
+            float offset = row % 2 == 0 ? 0 : tileWidth / 2;
+            if (row < rows) {
+                // y-Richtung (bzw. z)
+                for (int col = 0; col <= columns; col++) {
+                    float px = pos.x+ tileWidth*col + offset;
+                    Gizmos.DrawLine(new Vector3(px, 0.0f, posZ),
+                                    new Vector3(px, 0.0f, posZ + tileWidth));
+                }
+            }
+        }
+        
+    }
+}
---
+++ unity/Assets/Scripts/levelGeneration/HexGridEditor.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+[CustomEditor (typeof(HexGrid))]
+public class HexGridEditor : Editor {
+
+    private const char ADD = 'a';
+    private const char REMOVE = 'd';
+
+    HexGrid grid;
+
+    public void OnEnable() {
+        grid = (HexGrid)target;
+        SceneView.onSceneGUIDelegate += GridUpdate;
+    }
+
+    void GridUpdate(SceneView sceneView) {
+        Event e = Event.current;
+        Ray r = Camera.current.ScreenPointToRay(new Vector3(e.mousePosition.x, -e.mousePosition.y + Camera.current.pixelHeight));
+        Vector3 mousePos = r.origin;
+
+        if (e.isKey) {
+            if (e.character == ADD) {
+                Debug.Log("Add Tile");
+                GameObject obj;
+                Object prefab = PrefabUtility.GetPrefabParent(Selection.activeObject);
+                if (prefab) {
+                    obj = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
+                    obj.transform.position = new Vector3(mousePos.x, 0.0f, mousePos.z);
+
+                    obj.transform.localScale = new Vector3(grid.tileWidth * (obj.transform.localScale.x / grid.tileWidth), obj.transform.localScale.y, obj.transform.localScale.z);
+                }
+            } else if (e.character == REMOVE) {
+                Debug.Log("Remove Tile");
+            }
+        }
+    }
+
+    public override void OnInspectorGUI() {
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Tile Width");
+        grid.tileWidth = Mathf.Max(1, EditorGUILayout.FloatField(grid.tileWidth, GUILayout.Width(150)));
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Rows");
+        grid.rows = Mathf.Max(1, EditorGUILayout.IntField(grid.rows, GUILayout.Width(150)));
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Columns");
+        grid.columns = Mathf.Max(1, EditorGUILayout.IntField(grid.columns, GUILayout.Width(150)));
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Grid Color");
+        grid.color = EditorGUILayout.ColorField(grid.color, GUILayout.Width(150));
+        GUILayout.EndHorizontal();
+        
+        SceneView.RepaintAll();
+    }
+}
