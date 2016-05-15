43e994652256b9ecdd4cd3dd8205a5c21c4b039a
Matthaeus Baur <matthaeus.baur@makiart.de>
2016-05-15 12:38:41 +0200

    Laden und auswählen der Prefabs für das Hexgrid
    Positionierung funktioniert bisher aber nur mit der ersten Reihe

--- unity/Assets/Scripts/levelGeneration/HexGrid.cs
+++ unity/Assets/Scripts/levelGeneration/HexGrid.cs
@@ -17,16 +17,24 @@ public class HexGrid : MonoBehaviour {
     public int rows = 10;
     public int columns = 10;
     public float offset = 0.0f;
+    public float pointSize = 1.0f;
     public Color color = Color.red;
     
     private float tileHeight;
     private float tileWidth;
+    public float TileWidth {
+        get {
+            return tileWidth;
+        }
+    }
+
+    public Vector3[,] points;
 
     void OnDrawGizmos() {
         Gizmos.color = color;
         Vector3 pos = this.transform.position;
 
-        float gridWidth = columns * tileWidth;
+        points = new Vector3[columns, rows];
 
         for (int col = 0; col < columns; ++col) {
             float posX = pos.x + col * tileWidth;
@@ -35,6 +43,7 @@ public class HexGrid : MonoBehaviour {
 
                 Vector3 center = new Vector3(posX + (row % 2 == 0 ? tileWidth / 2 : 0), posZ, offset);
                 drawHex(center);
+                points[col, row] = center;
             }
         }
     }
@@ -42,12 +51,40 @@ public class HexGrid : MonoBehaviour {
     private void drawHex(Vector3 center) {
         for(int i = 0; i <= 6; ++i) {
             Gizmos.DrawLine(hexCorner(center, i), hexCorner(center, i == 6 ? 0 : i + 1));
+            drawPoint(center);
         }
     }
 
     private Vector3 hexCorner(Vector3 center, int i) {
         float angleDeg = 60 * i + 30;
         float angleRad = Mathf.PI / 180 * angleDeg;
-        return new Vector3(center.x + size * Mathf.Cos(angleRad), 0.0f, center.y + size * Mathf.Sin(angleRad));
+        return new Vector3(center.x + size * Mathf.Cos(angleRad), center.z, center.y + size * Mathf.Sin(angleRad));
+    }
+
+    private void drawPoint(Vector3 center) {
+        Gizmos.DrawLine(new Vector3(center.x - pointSize / 2, center.z, center.y), new Vector3(center.x + pointSize / 2, center.z, center.y));
+        Gizmos.DrawLine(new Vector3(center.x, center.z, center.y - pointSize / 2), new Vector3(center.x, center.z, center.y + pointSize / 2));
+    }
+
+    public Vector3 FindNextPoint(Vector3 mousePos) {
+        Debug.Log("find nearest hex to " + mousePos);
+        Debug.DrawLine(new Vector3(mousePos.x - pointSize / 2, mousePos.z, mousePos.y), new Vector3(mousePos.x + pointSize / 2, mousePos.z, mousePos.y));
+        Debug.DrawLine(new Vector3(mousePos.x, mousePos.z, mousePos.y - pointSize / 2), new Vector3(mousePos.x, mousePos.z, mousePos.y + pointSize / 2));
+
+        float distance = tileWidth;
+        Vector3 nearestPoint = new Vector3(0f,0f,0f);
+        for (int i = 0; i < points.GetLength(0); ++i) {
+            for (int j = 0; j < points.GetLength(1); ++j) {
+                //Debug.Log(j);
+                float d = Vector3.Distance(points[i, j], mousePos);
+                if (d < distance) {
+                    distance = d;
+                    nearestPoint = points[i, j];
+                    Debug.Log("x: " + i + ", y: " + j);
+                }
+            }
+        }
+        
+        return nearestPoint;
     }
 }
--- unity/Assets/Scripts/levelGeneration/HexGridEditor.cs
+++ unity/Assets/Scripts/levelGeneration/HexGridEditor.cs
@@ -9,11 +9,18 @@ public class HexGridEditor : Editor {
     private const char ADD = 'a';
     private const char REMOVE = 'd';
 
+    private int selectedPrefab = 0;
+
+    private List<GameObject> prefabs = new List<GameObject>();
+
+    private string prefabPath = "Assets/Scripts/levelGeneration";
+
     HexGrid grid;
 
     public void OnEnable() {
         grid = (HexGrid)target;
         SceneView.onSceneGUIDelegate += GridUpdate;
+        updatePrefabslist();
     }
 
     void GridUpdate(SceneView sceneView) {
@@ -24,17 +31,28 @@ public class HexGridEditor : Editor {
         if (e.isKey) {
             if (e.character == ADD) {
                 Debug.Log("Add Tile");
-                //GameObject obj;
-                //Object prefab = PrefabUtility.GetPrefabParent(Selection.activeObject);
-                //if (prefab) {
-                //    obj = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
-                //    obj.transform.position = new Vector3(mousePos.x, 0.0f, mousePos.z);
+                Vector3 prefabPos = grid.FindNextPoint(new Vector3(mousePos.x, 0.0f, mousePos.z));
+                GameObject obj;
+                Object prefab = prefabs[selectedPrefab]; //PrefabUtility.GetPrefabParent(Selection.activeObject);
+                if (prefab) {
+                    obj = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
+                    obj.transform.position = new Vector3(prefabPos.x, 0.0f, prefabPos.z); // new Vector3(mousePos.x, 0.0f, mousePos.z);
 
-                //    obj.transform.localScale = new Vector3(grid.tileWidth * (obj.transform.localScale.x / grid.tileWidth), obj.transform.localScale.y, obj.transform.localScale.z);
-                //}
+                    obj.transform.localScale = new Vector3(grid.TileWidth * (obj.transform.localScale.x / grid.TileWidth), obj.transform.localScale.y, obj.transform.localScale.z);
+                }
             } else if (e.character == REMOVE) {
                 Debug.Log("Remove Tile");
             }
+        }
+    }
+
+    private void updatePrefabslist() {
+        prefabs.Clear();
+        string[] search_results = System.IO.Directory.GetFiles(prefabPath, "*.prefab", System.IO.SearchOption.AllDirectories);
+        for (int i = 0; i < search_results.Length; i++) {
+            Object prefab = null;
+            prefab = AssetDatabase.LoadAssetAtPath(search_results[i], typeof(GameObject));
+            prefabs.Add(prefab as GameObject);
         }
     }
 
@@ -60,8 +78,29 @@ public class HexGridEditor : Editor {
         GUILayout.EndHorizontal();
 
         GUILayout.BeginHorizontal();
+        GUILayout.Label("Point Size");
+        grid.pointSize = Mathf.Max(1, EditorGUILayout.FloatField(grid.pointSize, GUILayout.Width(150)));
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
         GUILayout.Label("Grid Color");
         grid.color = EditorGUILayout.ColorField(grid.color, GUILayout.Width(150));
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Path to Tiles");
+        prefabPath = GUILayout.TextField(prefabPath);
+        if (GUILayout.Button("Update")) {
+            updatePrefabslist();
+        }
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        GUIContent[] content = new GUIContent[prefabs.Count];
+        for (int i = 0; i < prefabs.Count; ++i) {
+            content[i] = new GUIContent(prefabs[i].name);
+        }
+        selectedPrefab = GUILayout.SelectionGrid(selectedPrefab, content, 1);
         GUILayout.EndHorizontal();
 
         SceneView.RepaintAll();
